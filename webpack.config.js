fecaf0ca8a25ba59e5d8fe01ca82ff1634f6f55f
Juho Vepsalainen <bebraw@gmail.com>
2017-07-14 15:59:51 +0100

    chore: Set up style extraction

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,10 @@ const commonConfig = merge([
     ],
   },
   parts.lintJavaScript({ include: PATHS.app }),
-  parts.loadCSS(),
 ]);
 
 const productionConfig = merge([
+  parts.extractCSS({ use: 'css-loader' }),
 ]);
 
 const developmentConfig = merge([
@@ -37,6 +37,7 @@ const developmentConfig = merge([
     host: process.env.HOST,
     port: process.env.PORT,
   }),
+  parts.loadCSS(),
 ]);
 
 module.exports = (env) => {
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 exports.devServer = ({ host, port } = {}) => ({
   devServer: {
     historyApiFallback: true,
@@ -40,3 +42,29 @@ exports.loadCSS = ({ include, exclude } = {}) => ({
     ],
   },
 });
+
+
+exports.extractCSS = ({ include, exclude, use }) => {
+  // Output extracted CSS to a file
+  const plugin = new ExtractTextPlugin({
+    filename: '[name].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [ plugin ],
+  };
+};
