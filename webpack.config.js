c8bb10f4d650b71982b269297c8d11018593167d
Juho Vepsalainen <bebraw@gmail.com>
2017-07-14 15:10:08 +0100

    chore: Add WDS

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const commonConfig = {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that.
@@ -22,4 +22,44 @@ module.exports = {
       title: 'Webpack demo',
     }),
   ],
+};
+
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. Good for complex setups.
+      historyApiFallback: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+    },
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  );
+};
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig();
 };
